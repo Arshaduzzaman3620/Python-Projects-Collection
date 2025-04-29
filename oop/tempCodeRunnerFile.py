bf4a554413558c53750bf8d416fdfc7665bfa0a7
Arshad3620 <ridoy3620@gmail.com>
2025-04-29 21:03:07 +0900

    ad the oop folder

---
+++ oop/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokémon Name", ["Pikachu", "Squirtle", "Bulbasaur"])
+table.add_column("Type", ["Fire", "Water", "Grass"])
+print(table)
