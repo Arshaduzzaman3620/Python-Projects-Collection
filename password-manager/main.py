1b95b94257914deeca87a8b12ebd662d502a2ffc
Arshad3620 <ridoy3620@gmail.com>
2025-06-25 21:49:21 +0900

    Feat:add the write function

--- password-manager/main.py
+++ password-manager/main.py
@@ -3,7 +3,22 @@ from tkinter import *
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
-# ---------------------------- SAVE PASSWORD ------------------------------- #
+# ---------------------------- SAVE PASSWORD ----------------------- #
+def save ():
+  website = website_entry.get()
+  email = email_entry.get()
+  password = password_entry.get()
+
+
+
+
+  with open("data.txt", "a") as data_file:
+    data_file.write(f"{website} | {email} | {password}\n")
+  website_entry.delete(0, END)  # Clear the website entry field
+  password_entry.delete(0, END)  # Clear the password entry field
+  website_entry.focus()  # Set focus back to the website entry field
+
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
@@ -38,7 +53,7 @@ password_entry.grid(row=3, column=1, columnspan=2)
 # Buttons
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command= save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
